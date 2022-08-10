b1f269e9bcba94286cd51862dafbb0379b781da4
Jonatan Santa Cruz <scsjonatan@gmail.com>
2022-08-10 09:20:21 -0500

    feature: add image to home section (#4)

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Waves from '../../components/Waves/index.jsx'
 import './index.css'
+import colors from '../../common/styles/colors'
+import peaceAndLoveEmoji from '../../assets/images/emojis/peace-and-love.png'
 
 const Home = () => {
-  const [hue, setHue] = useState(0)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setHue(hue + 1)
-    }, 500)
-  }, [hue])
-
   return (
     <section>
-      <Waves color={`hsl(${hue}, 100%, 75%)`} />
-      <div className='home_container'>
-        <h1>Jonatan Santa Cruz</h1>
-        <h2>Frontend Developer</h2>
+      <Waves color={colors.colorPrimaryDark} />
+      <div className='home-container container'>
+        <div className='row center'>
+          <div className='col-6'>
+            <img alt='Jonatan Santa Cruz' src={peaceAndLoveEmoji} />
+          </div>
+          <div className='col-6 personal-data'>
+            <h1>Jonatan Santa Cruz</h1>
+            <h2>Frontend Developer</h2>
+            <p>
+              Passion to <b>learn</b>, <b>develop</b> and <b>create</b>.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   )
