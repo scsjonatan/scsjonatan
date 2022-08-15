5fe29d79d794a6242031beb22870eea7fd7aca1b
Jonatan Santa Cruz <scsjonatan@gmail.com>
2022-08-15 09:35:05 -0500

    feature: add skill section (#7)

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,15 @@ import './index.css'
 import Experience from './views/Experience/index.jsx'
 import Welcome from './views/Welcome/index.jsx'
 import Contact from './views/Contact/index.jsx'
+import Skills from './views/Skills/index.jsx'
 
 const App = () => {
   return (
     <>
       <Home />
       <Welcome />
+      <Skills />
       <Experience />
-      <div className='divider' />
       <Contact />
     </>
   )
