8c2dba2d00d372d3f57bcd74ee273392ac0fbf36
Jonatan Santa Cruz <jonatansantacruz@Jonatans-iMac.local>
2022-08-15 19:39:43 -0500

    chore: update dependencies and resize images

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import Home from './views/Home/index.jsx'
 import './index.css'
 import Experience from './views/Experience/index.jsx'
 import Welcome from './views/Welcome/index.jsx'
 import Contact from './views/Contact/index.jsx'
 import Skills from './views/Skills/index.jsx'
+
+const container = document.getElementById('app')
+const root = createRoot(container)
 
 const App = () => {
   return (
@@ -19,4 +22,5 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+root.render(<App />)
+
